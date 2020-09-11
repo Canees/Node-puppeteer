9fa88a0e0c54a043a159ddccc54ad69f31e9e11f
Caner <5658514@qq.com>
2020-09-11 16:48:41 +0800

    Signed-off-by: Caner <5658514@qq.com>

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const puppeteer = require('puppeteer')
 try {
-  (async (newurl = 'https://v.qq.com/x/cover/mzc00200ytgrst3.html') => {
+  ;(async (newurl = 'https://v.qq.com/x/cover/mzc00200ytgrst3.html') => {
     //打开浏览器
     const browser = await puppeteer.launch({
       headless: true,
       defaultViewport: null, //默认大小
-      args: ['--start-maximized'],
+      args: [
+        '--start-maximized',
+        '--no-sandbox',
+        // '--disable-infobars',
+        // '--no-zygote',
+        // '--no-first-run',
+        // '--disable-setuid-sandbox',
+        // '--disable-dev-shm-usage',
+        // '--disable-gpu=True',
+      ],
       ignoreDefaultArgs: ['--enable-automation'],
-      devtools: false
-    });
+      devtools: false,
+    })
     //新建标签
-    const page = await browser.newPage();
+    const page = await browser.newPage()
     //跳转指定页面
-    await page.goto(`https://www.sp-flv.com/?url=${newurl}`);
+    await page.goto(`https://www.sp-flv.com/?url=${newurl}`)
     // 监听返回->利用原页面方法解析返回的数据
     page.on('response', async function (response) {
       // 过滤返回接口
@@ -22,14 +31,17 @@ try {
         // 利用页面方法解析数据
         if (data.code == 200) {
           let urls = await page.evaluate((data) => {
-            let CryptoJS = window.CryptoJS;
-            let key = data.key;
-            let iv = data.iv;
-            let str = data.url;
-            let keys = CryptoJS.enc.Utf8.parse(key);// 秘钥
-            let ivv = CryptoJS.enc.Utf8.parse(iv);//向量iv
-            let decrypted = CryptoJS.AES.decrypt(str, keys, { iv: ivv, padding: CryptoJS.pad.ZeroPadding });//解码
-            let rest = decrypted.toString(CryptoJS.enc.Utf8);
+            let CryptoJS = window.CryptoJS
+            let key = data.key
+            let iv = data.iv
+            let str = data.url
+            let keys = CryptoJS.enc.Utf8.parse(key) // 秘钥
+            let ivv = CryptoJS.enc.Utf8.parse(iv) //向量iv
+            let decrypted = CryptoJS.AES.decrypt(str, keys, {
+              iv: ivv,
+              padding: CryptoJS.pad.ZeroPadding,
+            }) //解码
+            let rest = decrypted.toString(CryptoJS.enc.Utf8)
             return rest
           }, data)
           // // 关闭标签
@@ -47,11 +59,12 @@ try {
           // // await page2.keyboard.press('Enter');
           // // 采用video标签操作
           console.log(88, urls)
+          await browser.close() //关闭浏览器
         } else {
-          await browser.close();//关闭浏览器
+          await browser.close() //关闭浏览器
         }
       } else {
-        await browser.close();//关闭浏览器
+        await browser.close() //关闭浏览器
       }
     })
   })()
